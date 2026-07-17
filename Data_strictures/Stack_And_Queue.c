4dcff7d8931d728654d506c301ff6ec4e22ffd06
aioiuuu <q_wr28857wfu@outlook.com>
2026-07-17 18:07:24 +0800

    feat: Stack_And_Queue 新增队列顺序实现及测试

--- Data_strictures/Stack_And_Queue.c
+++ Data_strictures/Stack_And_Queue.c
@@ -89,4 +89,175 @@ typedef int ElemType;
 //
 // }
 
-//todo:栈的链式结构实现
+// //todo:栈的链式结构实现
+// typedef struct stack {
+//     ElemType data;
+//     struct stack *next;
+// }Stack;
+//
+// Stack* initStack() {
+//     Stack* s = (Stack*)malloc(sizeof(Stack));
+//     s->data = 0;
+//     s->next = NULL;
+//     return s;
+// }
+//
+// //todo:判断栈是否为空
+// int isEmpty(Stack* s) {
+//     if (s->next == NULL) {
+//         printf("empty\n");
+//         return 1;
+//     }
+//     else {
+//         return 0;
+//     }
+// }
+//
+// //todo:进栈/压栈(相当于链表中的头插法)
+// int push(Stack* s, ElemType e) {
+//     Stack* temp = (Stack*)malloc(sizeof(Stack));
+//     temp->data = e;
+//     temp->next = s->next;
+//     s->next = temp;
+//     return 1;
+// }
+//
+// //todo:出栈(相当于删除头节点的后一个数据)
+// int pop(Stack* s, ElemType* e) {
+//     if (s->next == NULL) {
+//         printf("empty\n");
+//         return 0;
+//     }
+//     *e = s->next->data;
+//     Stack* temp = s->next;
+//     s->next = temp->next;
+//     free(temp);
+//     return 1;
+// }
+//
+// //todo:获取栈顶元素
+// int getTop(Stack* s, ElemType* e) {
+//     if (s->next == NULL) {
+//         printf("empty\n");
+//         return 0;
+//     }
+//     *e = s->next->data;
+//     return 1;
+// }
+
+//todo:队列(queue)
+//todo:先进先出
+//队列（queue）是一种先进先出的线性表
+//它只允许在表的一段进行插入，而在另一段删除元素
+//在队列中，允许插入的一段称为队尾，允许删除的一段称为队头
+
+
+#define MAXSIZE 100
+typedef int ElemType;
+
+typedef struct {
+    ElemType data[MAXSIZE];
+    int front;
+    int rear;
+}Queue;
+
+//todo:初始化
+void initQueue(Queue* Q) {
+    Q->front = 0;
+    Q->rear = 0;
+}
+
+//todo:判断队列是否为空
+int isEmpty(Queue* Q) {
+    if (Q->front == Q->rear) {
+        printf("Queue is empty\n");
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+//todo:出队
+ElemType dequeue(Queue* Q) {
+    if (Q->front == Q->rear) {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    ElemType e = Q->data[Q->front];
+    Q->front++;
+    return e;
+}
+
+//todo:入队
+int queueFull(Queue* Q) {
+    if (Q->front > 0) {
+        int step = Q->front;
+        for (int i = Q->front; i <= Q->rear; ++i) {
+            Q->data[i-step] = Q->data[i];
+        }
+        Q->front = 0;
+        Q->rear = Q->rear - step;
+        return 1;
+    }
+    else {
+        printf("full");
+        return 0;
+    }
+}
+
+int equeue(Queue* Q, ElemType e) {
+    if (Q->rear == Q->front) {
+        if (!queueFull(Q)) {
+            return 0;
+        }
+    }
+    Q->data[Q->rear] = e;
+    Q->rear++;
+    return 1;
+}
+
+//todo:获取队头数据
+int getHead(Queue* Q, ElemType* e) {
+    if (Q->front == Q->rear) {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    else {
+        * e = Q->data[Q->front];
+        return 1;
+    }
+}
+
+//todo:测试代码
+int main() {
+    Queue q;
+    initQueue(&q);
+    equeue(&q,10);
+    equeue(&q,20);
+    equeue(&q,30);
+    equeue(&q,40);
+    equeue(&q,50);
+
+    printf("%d\n",dequeue(&q));
+    printf("%d\n",dequeue(&q));
+    ElemType e;
+    getHead(&q,&e);
+    printf("%d\n",e);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
